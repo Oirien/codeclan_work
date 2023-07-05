bbc8170e0355385a3e53e2ae3fc880c00bc55944
Oirien <136370232+Oirien@users.noreply.github.com>
2023-07-05 12:41:35 +0100

    mar chicken

---
+++ week_01/day_3/search_function.py
@@ -0,0 +1,17 @@
+chickens = [
+     { "name": "Margaret","age": 2,"eggs": 0},
+     { "name": "Hetty", "age": 1, "eggs": 2 },
+     { "name": "Henrietta", "age": 3, "eggs": 1 },
+     { "name": "Audrey", "age": 2, "eggs": 0 },
+     { "name": "Mabel", "age": 5, "eggs": 1 }
+]
+
+# print(chickens[2]["eggs"])
+
+result = "Audrey not found!"
+
+for chicken in chickens:
+    if chicken["name"] == "Audrey":
+        result = chicken
+    
+print(result)
