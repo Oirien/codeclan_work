4534e460e1dd6fa681294fb883b1ad090cf1e630
Robert Mayo <robert.mayo.ch@gmail.com>
2023-07-06 12:07:39 +0100

    bug with weapon penetration

--- week_01/day_4/do_I_function.py
+++ week_01/day_4/do_I_function.py
@@ -34,8 +34,14 @@ def armor_lookup(ship_id):
     return ship_id["armor"]
 
 
+def penetration_lookup(ship_id):
+    return ship_id("weapon_penetration")
 
-
+def ship_firing(ship_1, ship_2):
+    return(HP_lookup(ship_1) - (((armor_lookup(ship_1)-penetration_lookup(ship_2)) * 0.1) *DMG_lookup(ship_2)))
+    
+print(ship_firing(deimos, valkyrie))
 # define a function that returns the value for Armor
 
-# define a function that takes two ships, one firing on the other, and calculating damage dealt, and hp reamining.
+# define a function that takes two ships, one firing on the other, 
+# and calculating damage dealt, and hp reamining.
